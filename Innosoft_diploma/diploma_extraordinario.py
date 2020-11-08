b0301270ffb96035795d4368b25285d6a757aab7
eemv <eemv99@gmail.com>
2020-11-08 12:13:40 +0100

    Interfaz y reorganizacion de codigo

--- Innosoft_diploma/diploma_extraordinario.py
+++ Innosoft_diploma/diploma_extraordinario.py
@@ -1,55 +1,78 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
 import sys
 import os
-from reportlab.pdfgen import canvas
-#TODO ESTO VA A SER SUSTITUIDO POR TKINTER
-#from PyQt5 import QtCore, QtGui, uic 
- 
-# Cargar nuestro archivo .ui
-#form_class = uic.loadUiType("formularioPDF.ui")[0]
- 
-#class MyWindowClass(QtGui.QMainWindow, form_class):
- #def __init__(self, parent=None):
-  #QtGui.QMainWindow.__init__(self, parent)
-  #self.setupUi(self)
-  
-  #self.btn_pdf.clicked.connect(self.generarPDF)
+import time
+from tkinter import *
 
-  
- # Evento del boton btn_pdf
-def generarPDF():
-  
-  # Guardo en las variables los datos de los textEdit Ingresados por el Usuario
-  #self.nombre = str(self.textNombre.toPlainText())
-  #self.apellido = str(self.textApellido.toPlainText())
-  #self.correo = str(self.textCorreo.toPlainText())
-  #self.curso = str(self.textCurso.toPlainText())
-  #self.pais = str(self.textPais.toPlainText())
-  #self.obs = str(self.textObs.toPlainText())
-  
-    
-  # Ruta donde quiero crear el PDF
-  c = canvas.Canvas("C:/Users/pbied/Desktop/Formulario.pdf")
-  c.drawString(100,750,"DIPLOMA EXTRAORDINARIO INNOSOFT")
-  c.drawString(100,700,("Nombre: "+ "Pedro"))
-  c.drawString(100,680,("Apellidos: "+ "Biedma Fresno"))
-  c.drawString(100,660,("Observaciones: "+ "Completo el reto de la jornada en el menor tiempo posible"))
-  c.drawString(100,620,("Fecha: "+ "05/11/2020"))
- 
+from reportlab.pdfgen import canvas
+
+def processPDF(nombre, apellidos,titulo,fecha):
+  c = canvas.Canvas("/home/blackylyzard/Formulario.pdf")
+  c.drawString(100, 750, "DIPLOMA EXTRAORDINARIO INNOSOFT")
+  c.drawString(100, 700, ("Nombre: " + str(nombre.get())))
+  c.drawString(100, 680, ("Apellidos: " + str(apellidos.get())))
+  c.drawString(100, 660, ("Observaciones: " + str(titulo.get())))
+  c.drawString(100, 620, ("Fecha: " + str(fecha.get())))
   c.save()
 
-  #PARA LINUX: os.system("evince /home/diego123/Escritorio/Formulario.pdf &")  
-  #PARA WINDOWS: os.system("start AcroRD32 ruta_y_archivo.pdf &")
-  os.system("start AcroRD32 C:/Users/pbied/Desktop/Formulario.pdf &") 
+  os.system("evince /home/blackylyzard/Formulario.pdf")
+
+def diplomasExt():
+  wind1 = Toplevel()
+
+  nombre = StringVar()
+  apellidos = StringVar()
+  titulo = StringVar()
+  fecha = StringVar(wind1, value=time.strftime("%d/%m/%y"))
+
+
+  label = Label(wind1, text="Nombre")
+  label.grid(row=0, column=0, sticky=W, padx=5, pady=5)
+
+  entry = Entry(wind1, textvariable=nombre)
+  entry.grid(row=0, column=1, padx=5, pady=5)
+
+  label2 = Label(wind1, text="Apellidos")
+  label2.grid(row=1, column=0, sticky=W, padx=5, pady=5)
+
+  entry2 = Entry(wind1, textvariable=apellidos)
+  entry2.grid(row=1, column=1, padx=5, pady=5)
+
+  label3 = Label(wind1, text="Título")
+  label3.grid(row=2, column=0, sticky=W, padx=5, pady=5)
+
+  entry3 = Entry(wind1, textvariable=titulo)
+  entry3.grid(row=2, column=1, padx=5, pady=5)
+
+  label4 = Label(wind1, text="Fecha")
+  label4.grid(row=3, column=0, sticky=W, padx=5, pady=5)
+
+  entry4 = Entry(wind1, textvariable=fecha)
+  entry4.grid(row=3, column=1, padx=5, pady=5)
+
+
+  boton = Button(wind1, text="Procesar", command=lambda:processPDF(nombre, apellidos, titulo, fecha))
+  boton.grid(row=4, column=2)
+
+
+def mainInterface():
+  root = Tk()
+  Button(root, text="Diplomas Asistencia", command=None).pack()
+
+  Button(root, text="Diplomas Extraordinarios", command=diplomasExt).pack()
+
+  root.mainloop()
+
+
+
   
  
 if __name__ == "__main__":
+  mainInterface()
  #TO DO: INICIAR LA VENTANA TKINTER CON ENTRADAS DE TEXTO Y UN BOTON CON LLAMADA AL METODO generarPDF()
  #app = QtGui.QApplication(sys.argv)
  #MyWindow = MyWindowClass(None)
  #MyWindow.show()
  #app.exec_()
- generarPDF()
