1ee56a1664ab329771c43bdddcd61708894b9353
Francisco Alé Palacios <fraalepal@alum.us.es>
2020-12-31 11:25:00 +0100

    Creación de tabla de pruebas y generación de nuevo tests.xlsx

--- innosoft_diplomas/tests.py
+++ innosoft_diplomas/tests.py
@@ -6,15 +6,33 @@ class DiplomaAutomaticoTestCase():
         return True
 
 if __name__ == "__main__":  
-    # Create a Pandas dataframe from the data.
-    df = pd.DataFrame({'Data': [10, 20, 30, 20, 15, 30, 45]})
+    # Crear un DataFrame de  Pandas a partir de los datos.
+    df = pd.DataFrame({'DNI': [111111111, 111111112, 111111113, 111111114, 111111115, 111111116, 111111117],
+                        'Apellidos': ["Alé Palacios", "Gata Fernández", "Biedma Fresno", "Yanes Ariza", "Losada Ostos", "Merino Verde", "Benavides Cuevas"],
+                        'Nombre': ["Francisco", "José Manuel", "Pedro", "Miguel", "Guillermo", "Enrique", "David"],
+                        'Uvus': ["fraalepal", "josgatfer", "pedbiefre", "migyanari", "guilosost", "enrmerver", "davbencue"],
+                        'Correo': ["fraalepal@us.es", "josgatfer@us.es", "pedbiefre@us.es", "migyanari@us.es", "guilosost@us.es", "enrmerver@us.es", "davbencue@us.es"],
+                        'Perfil': ["http://evidentia.test/20/profiles/view/1", "http://evidentia.test/20/profiles/view/2", "http://evidentia.test/20/profiles/view/3", "http://evidentia.test/20/profiles/view/4", "http://evidentia.test/20/profiles/view/5", "http://evidentia.test/20/profiles/view/6", "http://evidentia.test/20/profiles/view/7"],
+                        'Participación': ["ASSISTANCE", "ASSISTANCE", "ASSISTANCE", "ASSISTANCE", "ASSISTANCE", "ASSISTANCE", "ASSISTANCE"],
+                        'Comité': [None, None, "Sostenibilidad", None, "Logística", None, "Logística"],
+                        'Evidencia aleatoria': [None, None, None, None, None, None, None],
+                        'Horas de evidencia aleatoria': [None, None, None, None, None, None, None],
+                        'Eventos asistidos': [10, 10, 10, 7, 10, 4, 10],
+                        'Horas de asistencia': [10, 10, 10, 7, 10, 4, 10],
+                        'Reuniones asistidas': [None, None, None, None, None, None, None],
+                        'Bono de horas': [None, None, None, None, None, None, None],
+                        'Horas de reuniones': [None, None, None, None, None, None, None],
+                        'Evidencias registradas': [None, None, None, None, None, None, None],
+                        'Horas de evidencias': [None, None, None, None, None, None, None],
+                        'Horas en total': [10, 10, 10, 7, 10, 4, 10]})
+    
 
-    # Create a Pandas Excel writer using XlsxWriter as the engine.
+    # Crear un ExcelWriter a partir de XlsxWriter.
     writer = pd.ExcelWriter('tests.xlsx', engine='xlsxwriter')
 
-    # Convert the dataframe to an XlsxWriter Excel object.
+    # Convertir el DataFrame a un objeto Excel de XlsxWriter.
     df.to_excel(writer, sheet_name='Worksheet', index=False)
 
-    # Close the Pandas Excel writer and output the Excel file.
+    # Cerrar el Writer y devolver el fichero creado.
     writer.save()
 pass
